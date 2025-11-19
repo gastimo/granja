46599abd3254b0fcd8783134b50e7096150d444b
Gastimo <gaston.martino@gmail.com>
2025-11-18 21:41:43 -0300

    Cambios menores en código Arduino del fragmentador

--- vigia/fragmentador/fragmentador.ino
+++ vigia/fragmentador/fragmentador.ino
@@ -1,25 +1,29 @@
 #include <Adafruit_NeoPixel.h>
 
-#define PI 3.1415926535
-int pinLed = 6;
-int cantPixeles = 275;
-int pixelesRecibidos = 0;
-int noBrillo = 0;
-float maxBrilloR = 10;
-float maxBrilloG = 10;
-float maxBrilloB = 11;
+#define FRAGMENTADOR_ANCHO 11
+#define FRAGMENTADOR_ALTO 25
+#define MAX_BRILLO 255
+#define MAX_POSICION 25
+#define NULL_POSICION 99
+#define VELOCIDAD 115200
+#define PIN_LED 6
 
 int posX = 0;
 int posY = 0;
+int cantPixeles = FRAGMENTADOR_ANCHO * FRAGMENTADOR_ALTO;
+int pixelesRecibidos = 0;
+float maxBrilloR = 10;
+float maxBrilloG = 10;
+float maxBrilloB = 11;
 int valorR = int(maxBrilloR);
 int valorG = int(maxBrilloG);
 int valorB = int(maxBrilloB);
-
 byte datos[5];
-Adafruit_NeoPixel strip(cantPixeles, pinLed, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel strip(cantPixeles, PIN_LED, NEO_GRB + NEO_KHZ800);
+
 
 void setup() {
-  Serial.begin(115200);  // Debe coincidir con la velocidad definida en Processing
+  Serial.begin(VELOCIDAD);  // Debe coincidir con la velocidad definida en Processing
   strip.begin(); 
   strip.fill(strip.Color(0,0,0)); 
   strip.show();
@@ -32,14 +36,14 @@ void loop() {
     if (bytesLeidos == 5) {
       posX = int(datos[0]);
       posY = int(datos[1]);
-      if (posX < 25 && posY < 25) { 
-        valorR = int(datos[2]) * maxBrilloR / 255;
-        valorG = int(datos[3]) * maxBrilloG / 255;
-        valorB = int(datos[4]) * maxBrilloB / 255;
-        indice = (posX * 25) + posY;
+      if (posX < MAX_POSICION && posY < MAX_POSICION) { 
+        valorR = int(datos[2]) * maxBrilloR / MAX_BRILLO;
+        valorG = int(datos[3]) * maxBrilloG / MAX_BRILLO;
+        valorB = int(datos[4]) * maxBrilloB / MAX_BRILLO;
+        indice = (posX * FRAGMENTADOR_ALTO) + posY;
         strip.setPixelColor(indice, strip.Color(valorR, valorG, valorB));
       }
-      else if (posX == 99 && posY == 99) {
+      else if (posX == NULL_POSICION && posY == NULL_POSICION) {
         strip.fill(strip.Color(0,0,0)); 
         strip.show();
         break;
@@ -49,7 +53,7 @@ void loop() {
         break;
       }
     }
-    if (pixelesRecibidos >= cantPixeles/11) {
+    if (pixelesRecibidos >= cantPixeles / FRAGMENTADOR_ANCHO) {
       pixelesRecibidos = 0;
       strip.show();
     }
